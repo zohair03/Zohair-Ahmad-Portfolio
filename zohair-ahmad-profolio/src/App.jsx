d4fa10676c524253d194f5e87c46c0c7f2bdb02e
zohair03 <142157237+zohair03@users.noreply.github.com>
2025-01-15 23:23:39 +0530

    header

--- zohair-ahmad-profolio/src/App.jsx
+++ zohair-ahmad-profolio/src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import "./App.css";
 
-function App(){
+function App() {
   return (
     <div>
-      <img src="" alt="" />
+      <Header />
       <h1 className="">Zohair Ahmad Shaikh</h1>
       <button>Resume</button>
+      <Footer/>
     </div>
   );
 }
---
+++ zohair-ahmad-profolio/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <ul clasName="taps">
+        <li>Home</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
